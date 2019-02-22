7a55fbfb629c732f6d64dcd04133f8b8c956c45a
iamahsansheikh <sh.ahsan2003@gmail.com>
2019-02-22 14:48:26 -0500

    fixing more bugs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to website!');
+    expect(compiled.querySelector('h1').textContent).toContain('Ahsan Ijaz!');
   });
 });
